642a3aa8a834b77df691d7966b252aca460b6142
FocusedDeve <crepinoscript12@gmail.com>
2026-02-13 16:21:08 +0000

    ✨F01: implmentation des directives de la feature 01

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // state (état, données)
+  const [prenom, setPrenom] = useState("");
+
+  // comportements
+  const handleClick = () => {
+    if (prenom !== "") {
+      alert(`Bonjour ${prenom}`);
+    } else {
+      console.error("veuillez remplir le champ");
+    }
+  };
+
+  const handleOnchange = (event) => {
+    setPrenom(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.current.value;
+    setPrenom("");
+  };
+
+  // affichge (render)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Bonjour chez nous !</h1>
+      <br />
+      <h1>Connectez-vous</h1>
+
+      <form action="submit" onSubmit={handleSubmit}>
+        <input
+          required
+          onChange={handleOnchange}
+          placeholder="Entrez votre prénom"
+          value={prenom}
+        />
+        <button onClick={handleClick}>Accéder à votre espace</button>
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
