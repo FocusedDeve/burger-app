a6faf503efc4087ad67a5eb84485b3713a8ea326
FocusedDeve <crepinoscript12@gmail.com>
2026-02-13 20:28:59 +0000

    ✨ F02 : App > routing system implementation

---
+++ src/components/pages/error/ErrorPage.jsx
@@ -0,0 +1,15 @@
+export default function ErrorPage() {
+  // state
+
+  // comportements
+
+  // affichage
+
+  return (
+    <div>
+      <h1>ErrorPage</h1>
+      <br />
+      <button>Rétourner à la page d'accueil</button>
+    </div>
+  );
+}
---
+++ src/components/pages/order/OrderPage.jsx
@@ -0,0 +1,15 @@
+export default function OrderPage() {
+  // state
+
+  // comportements
+
+  // affichage
+
+  return (
+    <div>
+      <h1>Bonjour</h1>
+      <br />
+      <button>Déconnexion</button>
+    </div>
+  );
+}
